e167fd82100a92b3bdd7737170e792465c4b1c9b
mashedcorn <joseph2056@gmail.com>
2019-04-04 15:20:59 -0400

    improved movement, added item map
    added item/ room description map functionality.  game loop is now implemented

--- functions/location.m
+++ functions/location.m
@@ -3,34 +3,38 @@ function [coords] = location(x,y,map)
     switch y
         case {'north', 'forward'}
             
-            if map(x(1,1)-1,x(1,2)) == 0
+            if map(x(1,1)-1,x(1,2),1) == 0
                disp('you cannot go that way')
             else
                 coords(1,1) = x(1,1)-1;
+                AdvanceRoom(coords, map);
             end
             
         case {'south', 'backwards'}
             
-            if map(x(1,1)+1,x(1,2)) == 0
+            if map(x(1,1)+1,x(1,2),1) == 0
                 disp('you cannot go that way')
             else
                 coords(1,1) = x(1,1)+1;
+               AdvanceRoom(coords, map);
             end
             
         case {'east', 'right'}
             
-            if map(x(1,1),x(1,2)+1) == 0
+            if map(x(1,1),x(1,2)+1,1) == 0
                 disp('you cannot go that way')
             else
                 coords(1,2) = x(1,2)+1;
+                AdvanceRoom(coords, map);
             end
             
         case {'west', 'left'}
             
-            if map(x(1,1),x(1,2)-1) == 0
+            if map(x(1,1),x(1,2)-1,1) == 0
                  disp('you cannot go that way')
             else
                 coords(1,2) = x(1,2)-1;
+                AdvanceRoom(coords, map);
             end
     end
 
